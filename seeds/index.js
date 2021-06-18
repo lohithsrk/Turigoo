bbb330bf4dbb6fa0322c6181502c4e38e064d8a7
lohithsrk <srklohith05@gmail.com>
2021-06-18 12:36:25 +0530

    add mapbox popup

--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
@@ -30,8 +30,8 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             geometry: {
                 "coordinates": [
-                  80.219684,
-                  12.974598
+                  cities[random1000].longitude,
+                  cities[random1000].latitude
                 ],
                 "type": "Point"
             },
